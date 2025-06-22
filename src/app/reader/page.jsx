03b071d30ab47c14282d22265460bbe750dd8a45
vlad-khrychov <vladkhrychov@gmail.com>
2025-06-23 00:19:18 +0300

    Add Suspense

--- src/app/reader/page.jsx
+++ src/app/reader/page.jsx
@@ -1,10 +1,15 @@
-"use client"
+"use client";
 import Reader from "@/components/Reader";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function RegisterPage() {
   const searchParams = useSearchParams();
   const book = searchParams.get("book");
   const section = searchParams.get("section");
-  return <Reader book={book} section={section} />;
+  return (
+    <Suspense>
+      <Reader book={book} section={section} />
+    </Suspense>
+  );
 }
