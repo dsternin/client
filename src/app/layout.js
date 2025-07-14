51da95b935d4c7d2c0c2f71ede8d53f10bf73df4
vlad-khrychov <vladkhrychov@gmail.com>
2025-07-14 23:59:41 +0300

    add Suspense

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,24 +9,27 @@ import { AuthProvider } from "@/store/AuthContext";
 import AdminPanel from "@/components/AdminPanel";
 import { BookContextProvider } from "@/store/BookContext";
 import BookInfoPanel from "@/components/BookInfoPanel";
+import { Suspense } from "react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="layout">
         <AuthProvider>
-          <BookContextProvider>
-            <ThemeProvider theme={theme}>
-              {/* <CssBaseline /> */}
-              <div className="stickyHeaderWrapper">
-                <Header />
-                <BookInfoPanel />
-              </div>
-              {/* <AdminPanel /> */}
-              <main className="content">{children}</main>
-              <Footer />
-            </ThemeProvider>
-          </BookContextProvider>
+          <Suspense fallback={null}>
+            <BookContextProvider>
+              <ThemeProvider theme={theme}>
+                {/* <CssBaseline /> */}
+                <div className="stickyHeaderWrapper">
+                  <Header />
+                  <BookInfoPanel />
+                </div>
+                {/* <AdminPanel /> */}
+                <main className="content">{children}</main>
+                <Footer />
+              </ThemeProvider>
+            </BookContextProvider>
+          </Suspense>
         </AuthProvider>
       </body>
     </html>
