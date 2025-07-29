99852d0d5b2a7cc06847668aacad180785381300
vlad-khrychov <vladkhrychov@gmail.com>
2025-07-30 02:26:15 +0300

    all text by default

--- src/components/Reader/Reader.jsx
+++ src/components/Reader/Reader.jsx
@@ -17,7 +17,7 @@ export default function Reader() {
   const { setSection, setPoint } = useBookContext();
   const [fullDoc, setFullDoc] = useState(null);
   const [currentPage, setCurrentPage] = useState(0);
-  const [pageBlockSize, setPageBlockSize] = useState(50);
+  const [pageBlockSize, setPageBlockSize] = useState(-1);
   function updateBlockSize(value) {
     const newSize = parseInt(value);
     setPageBlockSize(newSize);
