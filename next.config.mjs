3384cd8b3b24ae66ed42eb52d94f0d3cdb00aa16
Vlad Khrychov <v.khrychov@karazin.ua>
2026-01-11 14:04:36 +0200

    add max limit for body

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    middlewareClientMaxBodySize: "50mb",
+  },
+};
 
 export default nextConfig;
